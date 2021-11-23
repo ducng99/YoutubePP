7b637e8ce7338edf9670b2dc39e2595144f562ee
Thomas Nguyen <tom@ducng.dev>
2021-11-24 12:30:29 +1300

    Update youtubepp.user.js

--- youtubepp.user.js
+++ youtubepp.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Youtube++
 // @namespace    maxhyt.youtubepp
-// @version      1.0
+// @version      1.0.0.1
 // @description  Add small features to Youtube
 // @author       Maxhyt
 // @match        https://www.youtube.com/*
@@ -36,19 +36,19 @@
     async function ShowDislikes() {
         let match = /\/watch\?v=([a-zA-Z0-9-_]+)/.exec(window.location.href);
         let dislikeText = document.body.querySelectorAll('a.yt-simple-endpoint > yt-formatted-string.ytd-toggle-button-renderer')[1];
+            
+        let likeBarContainer = document.body.querySelector('ytd-sentiment-bar-renderer');
+        likeBarContainer.removeAttribute('hidden');
         
         if (match && match[1] && lastVidID !== match[1]) {
             lastVidID = match[1];
             
-            let res = await (await fetch(`https://www.googleapis.com/youtube/v3/videos?id=${match[1]}&key=AIzaSyDFxwBJ7ULyiAmBDqyHyf6sb5s2b9DsGJU&part=statistics`)).json();
+            let res = await (await fetch(`https://www.googleapis.com/youtube/v3/videos?id=${match[1]}&key=<GOOGLE_API_KEY>&part=statistics`)).json();
             let likeCount = parseInt(res.items[0].statistics.likeCount);
             let dislikeCount = parseInt(res.items[0].statistics.dislikeCount);
             dislikeText.innerHTML = formatNumber(dislikeCount);
             
-            let likeBarContainer = document.body.querySelector('ytd-sentiment-bar-renderer');
-            
             if (likeBarContainer) {
-                likeBarContainer.removeAttribute('hidden');
                 let likePerc = Math.floor(likeCount / (likeCount + dislikeCount) * 100);
                 
                 likeBarContainer.querySelector('#like-bar').style.width = `${likePerc}%`;
@@ -83,15 +83,3 @@
         }
     }
 })();
-
-/*
-Old likebar
-<ytd-sentiment-bar-renderer id="sentiment" class="style-scope ytd-video-primary-info-renderer" system-icons="" style="width: 152px;">
-<div id="container" class="style-scope ytd-sentiment-bar-renderer">
-   <div id="like-bar" class="style-scope ytd-sentiment-bar-renderer" style="width: 98%;"></div>
-</div>
-<tp-yt-paper-tooltip position="top" class="style-scope ytd-sentiment-bar-renderer" role="tooltip" tabindex="-1" style="left: 7.93333px; top: -64px;"><!--css_build_mark:third_party.javascript.youtube_components.tp_yt_paper_tooltip.tp.yt.paper.tooltip.css.js--><!--css_build_scope:tp-yt-paper-tooltip--><!--css_build_styles:third_party.javascript.youtube_components.tp_yt_paper_tooltip.tp.yt.paper.tooltip.css.js--><div id="tooltip" class="style-scope tp-yt-paper-tooltip hidden">
-  2,907,238 / 38,659
-</div>
-</tp-yt-paper-tooltip>
-</ytd-sentiment-bar-renderer>*/
