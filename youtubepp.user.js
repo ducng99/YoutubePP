2837420871ace76d0b4f4225b9bca38f243221cc
Thomas Nguyen <tom@ducng.dev>
2021-11-25 23:51:28 +1300

    v1.0.3

--- youtubepp.user.js
+++ youtubepp.user.js
@@ -1,7 +1,8 @@
 // ==UserScript==
 // @name         Youtube++
 // @namespace    maxhyt.youtubepp
-// @version      1.0.2
+// @version      1.0.3
+// @license      AGPL-3.0
 // @description  Add small features to Youtube
 // @author       Maxhyt
 // @match        https://www.youtube.com/*
@@ -34,39 +35,46 @@
     let lastVidID = '';
     
     async function ShowDislikes() {
-        let match = /\/watch\?v=([a-zA-Z0-9-_]+)/.exec(window.location.href);
-        let likeText = document.body.querySelectorAll('a.yt-simple-endpoint > yt-formatted-string.ytd-toggle-button-renderer')[0];
-        let dislikeText = document.body.querySelectorAll('a.yt-simple-endpoint > yt-formatted-string.ytd-toggle-button-renderer')[1];
+        const match = /\/watch\?v=([a-zA-Z0-9-_]+)/.exec(window.location.href);
+        const likeText = document.body.querySelectorAll('a.yt-simple-endpoint > yt-formatted-string.ytd-toggle-button-renderer')[0];
+        const dislikeText = document.body.querySelectorAll('a.yt-simple-endpoint > yt-formatted-string.ytd-toggle-button-renderer')[1];
+        const dislikeTextDefault = dislikeText.innerHTML;
         
-        let likeBarContainer = document.body.querySelector('ytd-sentiment-bar-renderer');
-        likeBarContainer.removeAttribute('hidden');
+        const likeBarContainer = document.body.querySelector('ytd-sentiment-bar-renderer');
         
         if (match && match[1] && lastVidID !== match[1]) {
             lastVidID = match[1];
             
             dislikeText.innerHTML = "...";
-            let res = await (await fetch(`https://r0zmjt1mti.execute-api.ap-southeast-2.amazonaws.com/public/${match[1]}`)).json();
-            let likeCount = parseInt(res.likeCount);
-            let dislikeCount = parseInt(res.dislikeCount);
-            dislikeText.innerHTML = formatNumber(dislikeCount);
+            const res = await (await fetch(`https://r0zmjt1mti.execute-api.ap-southeast-2.amazonaws.com/public/${match[1]}`)).json();
+            const likeCount = parseInt(res.likeCount);
+            const dislikeCount = parseInt(res.dislikeCount);
             
-            let likeBarWidth = likeText.parentNode.parentNode.getBoundingClientRect().width + dislikeText.parentNode.parentNode.getBoundingClientRect().width + 8;
-            likeBarContainer.style.width = `${likeBarWidth}px`;
-            
-            if (likeBarContainer) {
-                let likePerc = Math.floor(likeCount / (likeCount + dislikeCount) * 100);
-                
-                likeBarContainer.querySelector('#like-bar').style.width = `${likePerc}%`;
-                let tooltip = likeBarContainer.querySelector('#tooltip');
-                tooltip.innerHTML = `${likeCount.toLocaleString("en-US")} / ${dislikeCount.toLocaleString("en-US")}`;
-                
-                likeBarContainer.querySelector('#container').addEventListener('mouseover', () => {
-                    tooltip.classList.remove('hidden');
-                });
-                likeBarContainer.querySelector('#container').addEventListener('mouseleave', () => {
-                    tooltip.classList.add('hidden');
-                });
-            }            
+            if (likeCount > -1 && dislikeCount > -1) {
+                dislikeText.innerHTML = formatNumber(dislikeCount);
+                likeBarContainer.removeAttribute('hidden');
+
+                const likeBarWidth = likeText.parentNode.parentNode.getBoundingClientRect().width + dislikeText.parentNode.parentNode.getBoundingClientRect().width + 8;
+                likeBarContainer.style.width = `${likeBarWidth}px`;
+
+                if (likeBarContainer) {
+                    const likePerc = Math.floor(likeCount / (likeCount + dislikeCount) * 100);
+
+                    likeBarContainer.querySelector('#like-bar').style.width = `${likePerc}%`;
+                    const tooltip = likeBarContainer.querySelector('#tooltip');
+                    tooltip.innerHTML = `${likeCount.toLocaleString()} / ${dislikeCount.toLocaleString()}`;
+
+                    likeBarContainer.querySelector('#container').addEventListener('mouseover', () => {
+                        tooltip.classList.remove('hidden');
+                    });
+                    likeBarContainer.querySelector('#container').addEventListener('mouseleave', () => {
+                        tooltip.classList.add('hidden');
+                    });
+                }
+            }
+            else {
+                dislikeText.innerHTML = dislikeTextDefault;
+            }
         }
     }
     
